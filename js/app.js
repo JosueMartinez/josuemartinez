ec2728f2cd05cc4243e77aac799a8f1722fbb0cb
Josue Martinez <josuem@cementoscibao.com>
2016-10-05 15:53:19 -0400

    changes to review in production

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ui.bootstrap']);
+
+app.controller('GeneralController', function(){
+    this.fecha = new Date();
+
+    this.abrirResume = function(){
+        var pdf = '/resume/Resume.pdf';
+         $window.open(pdf);
+        alert('si');
+    };
+});
