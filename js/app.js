56951da1dc6ff3d532f9a4032d21a7940ad66a88
Josue Martinez <josuem@cementoscibao.com>
2016-10-20 15:03:27 -0400

    adding services

--- js/app.js
+++ js/app.js
@@ -3,9 +3,4 @@ var app = angular.module('app', ['ui.bootstrap']);
 app.controller('GeneralController', function(){
     this.fecha = new Date();
 
-    // this.abrirResume = function(){
-    //     var pdf = '/resume/Resume.pdf';
-    //      $window.open(pdf);
-    //     alert('si');
-    // };
 });
